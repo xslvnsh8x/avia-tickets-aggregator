40b1c71471968f3da715723ca02c12442ec0c5ce
Станислав Завьялов <stanislavzavalov@MacBook-Pro-Stanislav.local>
2022-08-09 20:20:40 +0500

    :sparkles: layout-header created

---
+++ components/layout-header/layout-header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header-wrapper">
+    <nuxt-link
+      to="/"
+      class="logo"
+    >
+      <img src="~/assets/images/logo.svg" alt="logo">
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "layout-header"
+}
+</script>
+
+<style scoped>
+.header-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  display: flex;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout">
+    <LayoutHeader/>
+
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+import LayoutHeader from '@/components/layout-header/layout-header';
+
+export default {
+  name: 'default',
+  components: {LayoutHeader}
+}
+
+
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.layout {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+}
+</style>
